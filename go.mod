d4e526213d86ea5a2a68a6b70c2c4c6f5a99757c
Daniyar Ketebay <daniyar.abc@gmail.com>
2020-09-23 16:39:56 +0600

    add config and logger

--- go.mod
+++ go.mod
@@ -1,3 +1,8 @@
 module github.com/luckytiger1/go-rest-api.git
 
 go 1.15
+
+require (
+	github.com/BurntSushi/toml v0.3.1
+	github.com/sirupsen/logrus v1.6.0
+)
--- internal/app/apiserver/apiserver.go
+++ internal/app/apiserver/apiserver.go
@@ -1,14 +1,40 @@
 package apiserver
 
-// APIServer
-type APIServer struct{}
+import "github.com/sirupsen/logrus"
 
-// New...
-func New() *APIServer {
-	return &APIServer{}
+// APIServer ...
+type APIServer struct {
+	config *Config
+	logger *logrus.Logger
 }
 
-// Start
-func (s *APIServer) Start() error  {
+// New ...
+func New(config *Config) *APIServer {
+	return &APIServer{
+		config: config,
+		logger: logrus.New(),
+	}
+}
+
+// Start ...
+func (s *APIServer) Start() error {
+
+	if err := s.configureLogger(); err != nil {
+		return err
+	}
+
+	s.logger.Info("starting api server")
+
+	return nil
+}
+
+func (s *APIServer) configureLogger() error {
+	level, err := logrus.ParseLevel(s.config.LogLevel)
+	if err != nil {
+		return err
+	}
+
+	s.logger.SetLevel(level)
+
 	return nil
 }
